625edcb63576e50aada93120dc63cee2d2579443
David Raynes <rayners@gmail.com>
2018-03-07 19:40:08 -0500

    nightwatch testing demo

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,20 +2,34 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-    'basic e2e tests': function (browser) {
-      // automatically uses dev Server port from /config.index.js
-      // default: http://localhost:8080
-      // see nightwatch.conf.js
-      const devServer = browser.globals.devServerURL
+    beforeEach: function(browser) {
+        const devServer = browser.globals.devServerURL
   
+        browser
+          .url(devServer)
+          .waitForElementVisible('#app', 5000);  
+    },
+    'basic e2e tests': function (browser) {
       browser
-        .url(devServer)
-        .waitForElementVisible('#app', 5000)
         .click('a[href="#/tasks"]')        
         .pause(5000)
         .assert.elementPresent('ul.list-group')
         .click('a[href="#/register"]')
         .pause(5000)
+        .assert.elementPresent('input[name="username"]')
         .end()
+    },
+
+    'adding a task': function(browser) {
+        browser
+            .click('a[href="#/tasks"]')
+            .pause(5000)
+            .setValue('input[name="newTask"]', "My new task")
+            .click('form button')
+            .pause(5000)
+            .assert.containsText('ul.list-group li:last-child', "My new task")
+            .click('ul.list-group li:last-child button.close')
+            .pause(5000)
+            .end()
     }
   }
